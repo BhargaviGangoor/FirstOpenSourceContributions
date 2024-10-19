8892c57e96d8d4f849c26a1523d0fc5b65b59097
BhargaviGangoor <154064565+BhargaviGangoor@users.noreply.github.com>
2024-10-19 12:08:21 +0530

    Update contributors.js
    Added Bhargavi Gangoor

--- assets/js/contributors.js
+++ assets/js/contributors.js
@@ -1,3 +1,4 @@
 window.contributors = [
     // { name: "Codebloom Studios", social: "https://linkedin.com/company/codebloomstudios", color: "#f26321" },
+    { name:"Bhargavi Gangoor",social: "www.linkedin.com/in/bhargavi-gangoor-857b9b294",color:"#f26321"}
 ];
